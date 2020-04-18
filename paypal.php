8481d0ed7bd0cb5b288e5e2b905d0c7a14420e60
client <client@gmail.com>
2020-04-18 07:29:34 -0800

    first commit

---
+++ paypal.php
@@ -0,0 +1,16 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_xclick">
+    <input type="hidden" name="business" value="codingbrains47-facilitator@gmail.com">
+    <input type="hidden" name="item_name" value="Donation">
+    <input type="hidden" name="item_number" value="1">
+    <input type="hidden" name="amount" value="9.00">
+    <input type="hidden" name="no_shipping" value="0">
+    <input type="hidden" name="no_note" value="1">
+    <input type="hidden" name="currency_code" value="USD">
+    <input type="hidden" name="lc" value="AU">
+    <input type="hidden" name="bn" value="PP-BuyNowBF">
+    <input type='hidden' name='cancel_return' value='http://site NAME/cancel.php'>
+    <input type='hidden' name='return' value='http://l/success.php'>
+    <input type="image" src="https://www.paypal.com/en_AU/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+    <img alt="" border="0" src="https://www.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+</form>
